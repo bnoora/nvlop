5cd8e78999d8377e5308fd04e5df59452c15bbf0
bnoora <bnooranii@gmail.com>
2024-01-20 15:29:02 -0800

    test for dbConfig

---
+++ tests/dbConfig.test.js
@@ -0,0 +1,11 @@
+const pool = require('../database/dbConfig');
+
+describe('Database Connection', () => {
+  it('should connect to the database successfully', async () => {
+    const client = await pool.connect();
+    expect(client).toBeDefined();
+    const res = await client.query('SELECT NOW()');
+    expect(res).toHaveProperty('rows');
+    client.release();
+  });
+});
