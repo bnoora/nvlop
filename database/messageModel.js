f7b85c1922e61655e6328aebb82b8b4b77d56016
bnoora <bnooranii@gmail.com>
2024-01-20 16:46:13 -0800

    feat: functins for getting channel messages and private messages from db

--- database/messageModel.js
+++ database/messageModel.js
@@ -35,4 +35,36 @@ async function createPrivateMessage(message_obj) {
     }
 }
 
-module.exports = {createMessage, createPrivateMessage};
+// Get all messages from the database by channel
+async function getMessagesByChannel(channel_id) {
+    const client = await pool.connect();
+    const query = 'SELECT * FROM messages WHERE channel_id = $1';
+    const values = [channel_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows;
+    } catch (err) {
+        console.error('Error getting messages', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+// Get private messages from the database by users
+async function getPrivateMessages(user_id_1, user_id_2) {
+    const client = await pool.connect();
+    const query = 'SELECT * FROM messages WHERE (user_id1 = $1 AND user_id2 = $2) OR (user_id1 = $2 AND user_id2 = $1)';
+    const values = [user_id_1, user_id_2];
+    try {
+        const res = await client.query(query, values);
+        return res.rows;
+    } catch (err) {
+        console.error('Error getting messages', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+module.exports = {createMessage, createPrivateMessage, getMessagesByChannel, getPrivateMessages};
