31b19e3d2e9987f3513173601782ef10d8a63a6f
bnoora <bnooranii@gmail.com>
2024-01-20 16:49:38 -0800

    test: added tests for retrieving messages

--- tests/database/messageModel.test.js
+++ tests/database/messageModel.test.js
@@ -1,4 +1,4 @@
-const { createMessage, createPrivateMessage } = require('../../database/messageModel'); 
+const {createMessage, createPrivateMessage, getMessagesByChannel, getPrivateMessages} = require('../../database/messageModel'); 
 const pool = require('../../database/dbConfig');
 
 const client = pool.connect();
@@ -29,4 +29,22 @@ describe('Message Model', () => {
         expect(res).toHaveProperty('user_id2');
         expect(res).toHaveProperty('message');
     });
+
+	it('should get all messages from the database by channel', async () => {
+		const res = await getMessagesByChannel(1);
+		expect(res).toHaveLength > 0;
+		expect(res[0]).toHaveProperty('message_id');
+		expect(res[0]).toHaveProperty('channel_id');
+		expect(res[0]).toHaveProperty('user_id');
+		expect(res[0]).toHaveProperty('message');
+	});
+
+	it('should get private messages from the database by users', async () => {
+		const res = await getPrivateMessages(1, 2);
+		expect(res).toHaveLength > 0;
+		expect(res[0]).toHaveProperty('message_id');
+		expect(res[0]).toHaveProperty('user_id1');
+		expect(res[0]).toHaveProperty('user_id2');
+		expect(res[0]).toHaveProperty('message');
+	});
 });
