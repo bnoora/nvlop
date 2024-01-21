71365455a91b2df8f600b657dfdb31ba7e101ba3
bnoora <bnooranii@gmail.com>
2024-01-21 14:28:58 -0800

    refactor: seperated private chat model from message model and added delete message functionality

--- tests/database/messageModel.test.js
+++ tests/database/messageModel.test.js
@@ -1,4 +1,4 @@
-const {createMessage, createPrivateMessage, getMessagesByChannel, getPrivateMessages} = require('../../database/messageModel'); 
+const {createMessage, getMessagesByChannel, deleteMessage} = require('../../database/messageModel'); 
 const pool = require('../../database/dbConfig');
 
 const client = pool.connect();
@@ -17,34 +17,18 @@ describe('Message Model', () => {
         expect(res).toHaveProperty('message');
     });
 
-    it('should create a private message in the database', async () => {
-        const message = {
-            user_id1: 1,
-            user_id2: 2,
-            message: 'Test message'
-        };
-        const res = await createPrivateMessage(message);
-        expect(res).toHaveProperty('message_id');
-        expect(res).toHaveProperty('user_id1');
-        expect(res).toHaveProperty('user_id2');
-        expect(res).toHaveProperty('message');
-    });
-
-	it('should get all messages from the database by channel', async () => {
+	it('should get max of 50 messages from the database by channel', async () => {
 		const res = await getMessagesByChannel(1);
 		expect(res).toHaveLength > 0;
+        expect(res).toHaveLength <= 50;
 		expect(res[0]).toHaveProperty('message_id');
 		expect(res[0]).toHaveProperty('channel_id');
 		expect(res[0]).toHaveProperty('user_id');
 		expect(res[0]).toHaveProperty('message');
 	});
 
-	it('should get private messages from the database by users', async () => {
-		const res = await getPrivateMessages(1, 2);
-		expect(res).toHaveLength > 0;
-		expect(res[0]).toHaveProperty('message_id');
-		expect(res[0]).toHaveProperty('user_id1');
-		expect(res[0]).toHaveProperty('user_id2');
-		expect(res[0]).toHaveProperty('message');
-	});
+    it('should delete a message from the database', async () => {
+        const res = await deleteMessage(1);
+        expect(res).toBeUndefined();
+    });
 });
