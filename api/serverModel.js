e19a5a3cac23a3d6d35d95fbd5279609b4f59bad
bnoora <bnooranii@gmail.com>
2024-01-22 17:52:57 -0800

    feat

---
+++ api/serverModel.js
@@ -0,0 +1,123 @@
+const pool = require('./dbConfig');
+
+// Get servers that a user is a member of
+async function getServersByUser(user_id) {
+    const client = await pool.connect();
+    const query = `SELECT * FROM servers 
+                    JOIN server_membership ON servers.server_id = server_membership.servers_id
+                    WHERE server_membership.user_id = $1`;
+    const values = [user_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows;
+    } catch (err) {
+        console.error('Error getting servers by user', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+// Add a user to a channel
+async function addUserToServer(data) {
+    const client = await pool.connect();
+    const {server_id, user_id} = data;
+    const query = 'INSERT INTO server_membership (servers_id, user_id) VALUES ($1, $2) RETURNING *';
+    const values = [server_id, user_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows[0];
+    } catch (err) {
+        console.error('Error adding user to channel', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+// Remove a user from a channel
+async function removeUserFromServer(data) {
+    const client = await pool.connect();
+    const {server_id, user_id} = data;
+    const query = 'DELETE FROM server_membership WHERE servers_id = $1 AND user_id = $2 RETURNING *';
+    const values = [server_id, user_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows[0];
+    } catch (err) {
+        console.error('Error removing user from channel', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+// Make a user a moderator of a channel
+async function addUserModerator(data) {
+    const client = await pool.connect();
+    const {server_id, user_id} = data;
+    const query = 'UPDATE server_membership SET is_mod = true WHERE servers_id = $1 AND user_id = $2 RETURNING *';
+    const values = [server_id, user_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows[0];
+    } catch (err) {
+        console.error('Error making user a moderator', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+// Remove a user as a moderator of a channel
+async function removeUserModerator(data) {
+    const client = await pool.connect();
+    const {server_id, user_id} = data;
+    const query = 'UPDATE server_membership SET is_mod = false WHERE servers_id = $1 AND user_id = $2 RETURNING *';
+    const values = [server_id, user_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows[0];
+    } catch (err) {
+        console.error('Error removing user as moderator', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+// Change the owner of a channel
+async function changeChannelOwner(data) {
+    const client = await pool.connect();
+    const {server_id, user_id} = data;
+    const query = 'UPDATE servers SET owner_id = $1 WHERE server_id = $2 RETURNING *';
+    const values = [user_id, server_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows[0];
+    } catch (err) {
+        console.error('Error changing channel owner', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+// Get mod status of a user in a channel
+async function getModStatus(data) {
+    const client = await pool.connect();
+    const {server_id, user_id} = data;
+    const query = 'SELECT is_mod FROM server_membership WHERE servers_id = $1 AND user_id = $2';
+    const values = [server_id, user_id];
+    try {
+        const res = await client.query(query, values);
+        return res.rows[0].is_mod;
+    } catch (err) {
+        console.error('Error getting mod status', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
+module.exports = {getServersByUser, addUserToServer, removeUserFromServer, addUserModerator, removeUserModerator, changeChannelOwner, getModStatus};
--- tests/api/channelModel.test.js
+++ tests/api/channelModel.test.js
@@ -5,79 +5,26 @@ const pool = require('../../api/dbConfig');
 
 
 describe('Channel Model', () => {
-    it('Should create a channel in the database', async () => {
-        const res = await createChannel(1, 'Test Channel', 'Test description');
-        expect(res).toHaveProperty('channel_id');
-        expect(res).toHaveProperty('channel_name');
-        expect(res).toHaveProperty('description');
+    it ('should create a channel', async () => {
+        const data = {channel_name: 'test', description: 'test', server_id: 1};
+        const res = await createChannel(data);
+        expect(res.channel_name).toEqual('test');
+        expect(res.description).toEqual('test');
+        expect(res.server_id).toEqual(1);
     });
 
-    it('Should get a channel from the database by channel_id', async () => {
-        const res = await getChannelById(2);
-        expect(res).toHaveProperty('channel_id');
-        expect(res).toHaveProperty('channel_name');
-        expect(res).toHaveProperty('description');
+    it ('should delete a channel', async () => {
+        const res = await deleteChannel(7);
+        expect(res).toEqual(undefined);
     });
 
-    it('Should get channels that a user is a member of', async () => {
-        const res = await getChannelsByUser(1);
-        expect(res).toHaveLength > 0;
-        expect(res[0]).toHaveProperty('channel_id');
-        expect(res[0]).toHaveProperty('channel_name');
-        expect(res[0]).toHaveProperty('description');
+    it ('should get a channel by id', async () => {
+        const res = await getChannelById(1);
+        expect(res.channel_name).toEqual('channel1');
+        expect(res.description).toEqual('channel1 description');
+        expect(res.server_id).toEqual(1);
     });
 
-    it('Should add a user to the channel', async () => {
-        const res = await addUserToChannel(1, 3);
-        expect(res).toHaveProperty('user_id');
-        expect(res).toHaveProperty('channel_id');
-    });
-
-    it('Should remove a user from the channel', async () => {
-        const res = await removeUserFromChannel(1, 3);
-        expect(res).toBeUndefined();
-    });
-
-    it('Should get the moderator status of a user', async () => {
-        const res = await getModStatus(1, 1);
-        expect(res).toBe(false);
-    });
-
-    it('Should add a user as a moderator of a channel', async () => {
-        const res = await addUserModerator(1, 1);
-        expect(res).toHaveProperty('user_id');
-        expect(res).toHaveProperty('channel_id');
-        expect(res).toHaveProperty('is_mod');
-        expect(res.is_mod).toBe(true);
-    });
-
-    it('Should remove a user as a moderator of a channel', async () => {
-        const res = await removeUserModerator(1, 1);
-        expect(res).toHaveProperty('user_id');
-        expect(res).toHaveProperty('channel_id');
-        expect(res).toHaveProperty('is_mod');
-        expect(res.is_mod).toBe(false);
-    });
-
-    it('Should change the owner of a channel', async () => {
-        const res = await changeChannelOwner(1, 3);
-        expect(res).toHaveProperty('channel_id');
-        expect(res).toHaveProperty('owner_id');
-        expect(res.owner_id).toBe(3);
-    });
-
-    it('Should reset the owner of a channel', async () => {
-        const res = await changeChannelOwner(1, 1);
-        expect(res).toHaveProperty('channel_id');
-        expect(res).toHaveProperty('owner_id');
-        expect(res.owner_id).toBe(1);
-    });
-
-    it('Should delete a channel from the database', async () => {
-        const res = await deleteChannel(3);
-        const res2 = await getChannelById(3);
-        expect(res2).toBeUndefined();
-    });
     afterAll(async () => {
         await pool.end();
     });
