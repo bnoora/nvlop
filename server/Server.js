fbf3d62c16a4d62544d45650768d1178c46bb051
bnoora <bnooranii@gmail.com>
2024-01-25 13:18:21 -0800

    feat: reorginized server and added new features

--- server/Server.js
+++ server/Server.js
@@ -3,16 +3,23 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
-const expressSession = require('express-session');
-const passport = require('passport');
+const authenticateToken = require('./config/authenticateToken');
+
 
 // Importing modules
 const socketHandler = require('./sockets/socketHandler');
-const initializePassport = require('./config/passportConfig');
 
 // Import Routes
 const authRoutes = require('./routes/auth');
+const channelRoutes = require('./routes/channelRouter');
+const messageRoutes = require('./routes/messageRouter');
+const privateChatRouter = require('./routes/privateChatRouter');
+const serverInviteRouter = require('./routes/serverInviteRouter');
+const serverRouter = require('./routes/serverRouter');
+const userRouter = require('./routes/userRouter');
+const userFriendRouter = require('./routes/userFriendRouter');
 
 // Initializing express app and socket server
 const app = express();
@@ -21,15 +28,6 @@ const io = new Server(server);
 const port = process.env.PORT || 3000;
 
 // Middleware
-initializePassport();
-app.use(expressSession({ 
-    secret: process.env.SESSION_SECRET, 
-    resave: false, 
-    saveUninitialized: false 
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(authRoutes);
 app.use(helmet());
 app.use(cors(
     {
@@ -37,13 +35,30 @@ app.use(cors(
         methods: ['GET', 'POST']
     }
 ));
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100000 // TODO: DEPLOYMENT: 100
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// non jwt routes
+app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
-    res.send('Chat server running...');
+    res.send('Server running...');
 });
+
+// jwt middleware
+app.use(authenticateToken);
+
+// jwt routes
+app.use('/api/channels', channelRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/privateChats', privateChatRouter);
+app.use('/api/serverInvites', serverInviteRouter);
+app.use('/api/servers', serverRouter);
+app.use('/api/users', userRouter);
+app.use('/api/userFriends', userFriendRouter);
 
 socketHandler(io);
 
