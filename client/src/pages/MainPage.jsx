13112e64549ee28e615f91eb6474d804b71d5a98
bnoora <bnooranii@gmail.com>
2024-01-26 14:46:17 -0800

    feat: modified main page to be the central component for the app

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,7 +1,53 @@
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+
+// Components
+import ServerBar from "../components/ServerBar";
+import FriendComponent from "../components/FriendComponent";
+import ServerComponent from "../components/ServerComponent";
+
+
+
 export default function MainPage() {
+    const { user } = useContext(AuthContext);
+    const { servers, friends } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    // STATES 
+    const [showFriendComponent, setShowFriendComponent] = useState(true);
+    const [showServerComponent, setShowServerComponent] = useState(false);
+    const [showAddServerForm, setShowAddServerForm] = useState(false);
+    const [showAddFriendForm, setShowAddFriendForm] = useState(false);
+    const [showAddChannelForm, setShowAddChannelForm] = useState(false);
+
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }
+    , [user, navigate]);
+
+
+    // HANDLERS FOR VIEWS
+    const handleToggleFriendComponent = () => {
+        setShowServerComponent(false);
+        setShowFriendComponent(true);
+    };
+
+    const handleToggleServerComponent = () => {
+        setShowFriendComponent(false);
+        setShowServerComponent(true);
+    };
+
     return (
         <div>
-            <h1>Main Page</h1>
+            <ServerBar onServerClick={handleToggleServerComponent} onFriendClick={handleToggleFriendComponent}/>
+            {showServerComponent && <ServerComponent servers={servers}/>}
+            {showFriendComponent && <FriendComponent friends={friends}/>}
         </div>
     );
 }
