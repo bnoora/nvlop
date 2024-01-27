6a348066f85b45064f56401e4a6e815eef0b88a4
bnoora <bnooranii@gmail.com>
2024-01-26 18:39:53 -0800

    feat: msg input component

---
+++ client/src/components/partials/MessageInput.jsx
@@ -0,0 +1,22 @@
+export default function MessageInput({ onSendMessage }) {
+    const [message, setMessage] = useState("");
+
+    const handleSendMessage = (e) => {
+        e.preventDefault();
+        onSendMessage(message);
+        setMessage("");
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSendMessage}>
+                <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
+}
