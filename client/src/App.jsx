8b8da1dc3d2236a522089aefab5675632f642b42
bnoora <bnooranii@gmail.com>
2024-02-16 21:45:31 -0800

    updated router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import React from 'react';
 import { useContext } from 'react';
+import { useEffect } from 'react';
 
 // Contexts
 import { AuthProvider, AuthContext } from './context/AuthContext';
@@ -17,12 +18,9 @@ function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Navigate replace to={isLoggedIn ? "/main" : "/login"} />} />
-				{isLoggedIn ? ( 
-					<Route path="/main" element={<MainPage />} />
-				) : (
-					<Route path="/login" element={<LoginPage />} />
-				)}
+				{/* <Route path="/" element={<Navigate replace to={isLoggedIn ? "/main" : "/login"} />} /> */}
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/main" element={<MainPage />} />
 			</Routes>
 		</Router>
 	);
