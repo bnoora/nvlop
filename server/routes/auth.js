3d2357684563e35e174879607876612968841288
bnoora <bnooranii@gmail.com>
2024-02-16 19:28:31 -0800

    changed logic for dev test

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,7 +11,6 @@ const {createSessionToken, removeSessionToken} = require('../api/sessionModel');
 
 // Login POST request
 router.post('/login', async (req, res) => {
-    console.log('login request');
     try {
         const { username, password } = req.body;
 
@@ -22,12 +21,12 @@ router.post('/login', async (req, res) => {
 
         const user = await checkLogin(username, password);
         if (user.success) {
-            const token = await generateWebToken(user.user_id);
+            const token = await generateWebToken(user.user.user_id);
             // Set the token as a cookie 
             res.cookie('token', token, {
-                httpOnly: true, // The cookie is not accessible via JavaScript
-                secure: true, 
-                sameSite: 'strict', 
+                httpOnly: false, // TODO: Change to true in production
+                secure: false, // TODO: Change to true in production
+                sameSite: 'none', // TODO: Change to 'strict' in production
                 maxAge: 24 * 60 * 60 * 10000 
             });
 
@@ -37,15 +36,13 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ message: 'Login failed', error: user.message });
         }
     } catch (err) {
-        console.error(err);
         return res.status(500).json({ message: 'Login failed due to server error', error: err.message });
     }
 });
     
 
 
-router.post('/register', (req, res) => {
-    console.log('Register Req')
+router.post('/register', async (req, res) => {
     const user_obj = req.body;
     try {
         const password = user_obj.password;
@@ -65,35 +62,41 @@ router.post('/register', (req, res) => {
 
         return res.status(200).json({ message: 'Registration successful', user: returnUser});
     } catch (err) {
-        console.error(err);
         return res.status(500).json({ error: err.message, message: 'Registration failed or user already exists'});
     }
 });
 
 
-router.post('/check-login', (req, res) => {
+router.post('/check-login', async (req, res) => {
     let token = null;
+    let userId = null;
+    console.log(req.cookies);
     if (req.cookies.token) {
         token = req.cookies.token;
-        console.log('Token found');
     } else {
-        console.log('No token found');
         return res.status(401).json({ message: "No authentication token found" });
     }
-
     if (!token) {
         return res.status(200).json({ isLoggedIn: false });
     }
-
+    console.log('Check login token');
+    console.log(token);
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
             // Token is invalid or expired
             return res.status(401).json({ isLoggedIn: false, message: "Invalid or expired token" });
         }
         userId = decoded.userId;
-        user = getUserById(userId);
-        return res.json({ isLoggedIn: true, user: user});
     });
+    if (!userId) {
+        return res.status(200).json({ isLoggedIn: false });
+    }
+
+    const user = await getUserById(userId);
+    if (!user) {
+        return res.status(200).json({ isLoggedIn: false });
+    }
+    return res.status(200).json({ isLoggedIn: true, user: user });
 });
 
 router.post('/logout', (req, res) => {
@@ -107,14 +110,13 @@ router.post('/socket-auth', async (req, res) => {
     if (!token) {
         return res.status(401).json({ message: "No authentication token found" });
     }
-
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: "Invalid or expired token" });
         }
         const sessionToken = crypto.randomBytes(64).toString('hex');
         const userId = decoded.userId;
-        createSessionToken({ user_id: userId, token: sessionToken });
+        createSessionToken(userId, sessionToken);
         return res.status(200).json({ message: "Socket authenticated", sessionToken: sessionToken });
     });
 });
