a58009b6a83cbce2ed482e3d573d397461a70b80
bnoora <bnooranii@gmail.com>
2024-02-21 14:24:13 -0800

    modified ServerIcon to the desired functionality

--- client/src/components/partials/ServerIcon.jsx
+++ client/src/components/partials/ServerIcon.jsx
@@ -1,7 +1,15 @@
-export default function ServerIcon({ server }) {
+export default function ServerIcon({ server, onClick }) {
+    function firstLetter(str) {
+        return str.charAt(0);
+    }
+    
     return (
-        <div>
-            <img src={server.iconurl} alt={server.name} />
+        <div onClick={onClick}>
+            {server.icon_url ? (
+                <img src={server.icon_url} alt={server.server_name} />
+            ) : (
+                <div>{firstLetter(server.server_name)}</div>
+            )} 
         </div>
     );
 }
