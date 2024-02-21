5352175f16268d93f2483cf5fdf6c4184e7544b3
bnoora <bnooranii@gmail.com>
2024-02-21 15:11:07 -0800

    added loaing and redone mapping with proper keys

--- client/src/components/ServerChannels.jsx
+++ client/src/components/ServerChannels.jsx
@@ -4,14 +4,18 @@ import ChannelRows from "./partials/ChannelRows";
 export default function ServerChannels(props) {
     const { channels, onChannelClick, onToggleForm } = props;
 
+    if (!channels) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
-            {channels.map((channel) => (
-                <div key={channel.id}>
-                    <ChannelRows channel={channel} onChannelClick={onChannelClick} />
-                </div>
-            ))}
             <button onClick={onToggleForm}>Add Channel+</button>
+            {
+                channels.map((channel) => (
+                    <ChannelRows key={channel.channel_id} channel={channel} onClick={onChannelClick} />
+                ))
+            }
         </div>
     );
 }
