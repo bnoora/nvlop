90b61a7d83b8aa756575c342e88e4da963e43098
bnoora <bnooranii@gmail.com>
2024-02-22 13:50:51 -0800

    fixed api call to add a channel and fixed inpujt tracking

--- client/src/components/partials/AddChannelForm.jsx
+++ client/src/components/partials/AddChannelForm.jsx
@@ -1,23 +1,22 @@
 import axios from "axios";
-import react, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function AddChannelForm({ server, onToggleForm }) {
-    const serverId = server.id;
-    const { channelName, setChannelName } = useState('');
-    const { channelDescription, setChannelDescription } = useState('');
+    const serverId = server.server_id;
+    const [ channelName, setChannelName ] = useState('');
+    const [ channelDescription, setChannelDescription ] = useState('');
 
 
 
     async function handleChannelAdd(event) {
         event.preventDefault();
-        const channelName = event.target.elements.channelName.value;
         if (!channelName) {
             return;
         }
         try {
-            const response = await axios.post('http://localhost:3001/create-channel', { serverId: serverId, 
-                                                                                        channelName: channelName,  
-                                                                                        channelDescription: channelDescription});
+            const response = await axios.post('http://localhost:3001/api/channels/create-channel', { server_id: serverId, 
+                                                                                        channel_name: channelName,  
+                                                                                        description: channelDescription});
             if (response.status === 200) {
                 channels.push(response.data.channel);
                 onToggleForm();
@@ -30,11 +29,19 @@ export default function AddChannelForm({ server, onToggleForm }) {
         }
     }
 
+    function handleChannelNameChange(event) {
+        setChannelName(event.target.value);
+    }
+
+    function handleChannelDescriptionChange(event) {
+        setChannelDescription(event.target.value);
+    }
+
     return (
         <div>
             <form onSubmit={handleChannelAdd}>
-                <input type="text" placeholder="Channel Name" onChange={(e) => setChannelName(e.target.value)} />
-                <input type="text" placeholder="Channel Description" onChange={(e) => setChannelDescription(e.target.value)} />
+                <input type="text" placeholder="Channel Name" onChange={handleChannelNameChange} />
+                <input type="text" placeholder="Channel Description" onChange={handleChannelDescriptionChange} />
                 <button type="submit">Add Channel</button>
                 <button type="button" onClick={onToggleForm}>Cancel</button>
             </form>
